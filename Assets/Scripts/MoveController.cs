8fd5e27ccafa3d1ba108920c598be140510f8924
msh0325 <113653680+msh0325@users.noreply.github.com>
2026-09-09 17:02:08 +0900

    Init Unity and Make InputAction
    unity 커밋
    그리고 플레이어 컨트롤러 Input System 이용해서 제작.

---
+++ Assets/Scripts/MoveController.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MoveController : MonoBehaviour
+{
+    private new Camera camera;
+    private Rigidbody rigid;
+    private Vector3 moveDirection = Vector3.zero;
+    private Vector3 c_forward;
+    private Vector3 c_right;
+    private Vector3 direction;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private LayerMask groundLayer;
+    void Start()
+    {
+        camera = Camera.main;
+        rigid = GetComponent<Rigidbody>();
+
+        c_forward = camera.transform.forward;
+        c_right = camera.transform.right;
+
+        c_forward.y = 0;
+        c_right.y = 0;
+
+        c_forward.Normalize();
+        c_right.Normalize();
+    }
+
+    public void OnMove(InputValue value)
+    {
+        Vector2 input = value.Get<Vector2>();
+        moveDirection = c_right * input.x + c_forward * input.y;
+    }
+
+    public void OnLook(InputValue value)
+    {
+        Vector2 mousePosition = value.Get<Vector2>();
+
+        Ray ray = camera.ScreenPointToRay(mousePosition);
+
+        if(Physics.Raycast(ray, out RaycastHit hit, 100f, groundLayer))
+        {
+            Vector3 targetPos = hit.point;
+
+            Vector3 lookDirection = targetPos - transform.position;
+            lookDirection.y = 0;
+
+            transform.rotation = Quaternion.LookRotation(lookDirection);
+        }
+
+    }
+
+    public void OnAttack(InputValue value)
+    {
+        Debug.Log("attack");
+        
+    }
+
+    void FixedUpdate()
+    {
+        direction = moveDirection * speed;
+        rigid.linearVelocity = new Vector3(direction.x, rigid.linearVelocity.y, direction.z);
+    }
+}
